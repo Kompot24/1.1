7cc3e13f8335005fb13bb9d22f807c713fbb4101
Kompot24 <efim.popov.2017@bk.ru>
2026-02-23 22:15:11 +0800

    second commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-           
+
+            Console.WriteLine("Число: ");
+            var numb = int.Parse(Console.ReadLine());
+            
+            int first = numb / 100;
+            int second = (numb - 100 * first) / 10;
+            int third = numb - (first * 100 + second * 10);
+            if (first >= second && first >= third)
+            {
+                Console.WriteLine($"Наибольшее число - {first}");
+            }
+            else if (second >= first && second >= third)
+            {
+                Console.WriteLine($"Наибольшее число - {second}");
+            }
+            else if (third >= first && third >= second)
+            {
+                Console.WriteLine($"Наибольшее число - {third}");
+            }
         }
 
     }
